81f4ba8f48a3ecc416950095d9de01b14b0c7a36
Ronak Radadiya <ronakradadiya7@gmail.com>
2022-03-29 08:28:13 +0530

    0 - 1 Knapsack Problem

---
+++ Recursion/0-1-knapsack.js
@@ -0,0 +1,30 @@
+class Solution 
+{
+    //Function to return max value that can be put in knapsack of capacity W.
+    knapSack(W, wt, val, n)
+    { 
+       // code here
+       return this.maxProfit(wt, val, 0, W, n);
+    }
+    
+    maxProfit(weights, profit, currentItem, capacity, n) {
+        if (currentItem === n) {
+            return 0;
+        }
+        
+        const currentItemWeight = weights[currentItem];
+        const currentItemProfit = profit[currentItem];
+        
+        let consider = 0;
+        if (currentItemWeight <= capacity) {
+            consider = currentItemProfit + this.maxProfit(weights, profit, currentItem + 1, capacity - currentItemWeight, n)
+        }
+        const notConsider = this.maxProfit(weights, profit, currentItem + 1, capacity, n);
+        return Math.max(consider, notConsider);
+    }
+}
+
+const s = new Solution();
+console.log(s.knapSack(4, [4,5,1], [1,2,3], 3));
+
+
