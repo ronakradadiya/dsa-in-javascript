7f31364e9029d4fbab32642fea84864069e96cf4
Ronak Radadiya <ronakradadiya7@gmail.com>
2025-08-24 02:03:33 -0700

    leetcode 344
    second approach - reverse string completed

--- arrays/344-reverse-string.js
+++ arrays/344-reverse-string.js
@@ -2,21 +2,29 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
+var reverseString = function (s) {
   let i = 0;
   let j = s.length - 1;
 
-  while (i < j) {
-      let temp = s[i];
-      s[i] = s[j];
-      s[j] = temp;
+  // while (i < j) {
+  //   let temp = s[i];
+  //   s[i] = s[j];
+  //   s[j] = temp;
 
-      i++;
-      j--;
+  //   i++;
+  //   j--;
+  // }
+
+  const n = s.length
+
+  for (let i = 0; i < Math.floor(n / 2); i++) {
+    let temp = s[i];
+    s[i] = s[n - 1 - i];
+    s[n - 1 - i] = temp;
   }
 
-  return s
+  return s;
 };
 
-console.log(reverseString(["h","e","l","l","o"]))
-console.log(reverseString(["H","a","n","n","a","h"]))
+console.log(reverseString(["h", "e", "l", "l", "o"]));
+console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
