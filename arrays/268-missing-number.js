253a3b6ae689fced29309297fce52262e01fdfc9
Ronak Radadiya <ronakradadiya7@gmail.com>
2025-09-08 21:58:16 -0700

    leetcode 268
    missing number completed

---
+++ arrays/268-missing-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    const n = nums.length;
+
+    const nTotal = (n * (n + 1)) / 2
+
+    const currentTotal = nums.reduce((num, acc) => num + acc, 0)
+
+    return nTotal - currentTotal
+};
+
+console.log(missingNumber([3,0,1]))
+console.log(missingNumber([0,1]))
