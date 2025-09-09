3b026feed049bb1f572e6e5263f76c0fc487e435
Ronak Radadiya <ronakradadiya7@gmail.com>
2025-09-08 21:28:44 -0700

    leetcpde 485 completed
    max consecutive zeroes problem completed

---
+++ arrays/485-max-consecutive-zeroes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+  let count = 0;
+  let maxCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 1) {
+          count++
+          if (count > maxCount) {
+              maxCount = count
+          }
+      } else {
+          count = 0;
+      }
+  }
+
+  return maxCount
+};
+
+console.log(findMaxConsecutiveOnes([1,1,0,1,1,1]))
+console.log(findMaxConsecutiveOnes([1,0,1,1,0,1]))
