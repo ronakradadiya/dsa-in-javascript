9f9d2d186b68a97948007c751a9bbc0d7570167f
Ronak Radadiya <ronakradadiya7@gmail.com>
2025-09-08 21:37:39 -0700

    added alternate solution to LC 485 problem

--- arrays/485-max-consecutive-zeroes.js
+++ arrays/485-max-consecutive-zeroes.js
@@ -18,7 +18,20 @@ var findMaxConsecutiveOnes = function (nums) {
   }
 
   return maxCount
+
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] === 1) {
+  //     count++;
+  //   } else {
+  //     maxCount = Math.max(count, maxCount);
+  //     count = 0;
+  //   }
+  // }
+
+  // return Math.max(count, maxCount)
 };
 
-console.log(findMaxConsecutiveOnes([1,1,0,1,1,1]))
-console.log(findMaxConsecutiveOnes([1,0,1,1,0,1]))
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
+console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1]));
+
+//  Check max count at the end also
