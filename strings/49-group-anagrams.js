9face49f0b53429b3f4663a7d49e9e8bda1e6d18
Ronak Radadiya <ronakradadiya7@gmail.com>
2025-09-29 18:22:00 -0700

    leetcode 49 - group anagrams - optimal approach completed

--- strings/49-group-anagrams.js
+++ strings/49-group-anagrams.js
@@ -2,27 +2,27 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
+var groupAnagrams = function (strs) {
   // n - no of strs
   // m - max length of characters of each string
-  let map = {}
+  let map = {};
 
-  for (let i = 0; i < strs.length; i++) { // O(n)
+  for (let i = 0; i < strs.length; i++) {// O(n)
     const sortedKey = strs[i].split("").sort().join(""); // O(mlogm)
 
     if (!map[sortedKey]) {
-      map[sortedKey] = [strs[i]]
+      map[sortedKey] = [strs[i]];
     } else {
-      map[sortedKey].push(strs[i])
+      map[sortedKey].push(strs[i]);
     }
   }
 
-  return Object.values(map)
+  return Object.values(map);
 };
 
-console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
-console.log(groupAnagrams([""]))
-console.log(groupAnagrams(["a"]))
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAnagrams([""]));
+console.log(groupAnagrams(["a"]));
 
 // Time complexity - O(n * mlogm)
 // Space complexity - O(n * m)
@@ -56,3 +56,38 @@ So total space to store all strings ≈ n × m.
 ✅ Example:
 6 strings × 3 characters each = 18 character units → scales as O(n × m).
 */
+
+var groupAnagrams1 = function (strs) {
+  // n - no of strs
+  // m - max length of characters of each string
+  let map = {};
+
+  for (let i = 0; i < strs.length; i++) { // O(n)
+    const freqArray = Array(26).fill(0); // O(26) -> O(1)
+    const str = strs[i];
+    for (let j = 0; j < str.length; j++) { // O(m)
+      const index = str[j].charCodeAt() - "a".charCodeAt();
+      freqArray[index] += 1;
+    }
+
+    let sortedKey = "";
+
+    for (let k = 0; k < freqArray.length; k++) { // O(26) -> O(1)
+      sortedKey = sortedKey + String.fromCharCode(k) + freqArray[k];
+    }
+
+    if (!map[sortedKey]) {
+      map[sortedKey] = [strs[i]];
+    } else {
+      map[sortedKey].push(strs[i]);
+    }
+  }
+
+  return Object.values(map);
+};
+
+console.log(groupAnagrams1(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAnagrams1([""]));
+console.log(groupAnagrams1(["a"]));
+// Time complexity - O(n * m)
+// Space complexity - O(n * m)
