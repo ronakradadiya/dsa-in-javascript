0b9105f1d43d784de118033e62b0c28d32f12882
Ronak Radadiya <ronakradadiya7@gmail.com>
2025-10-02 20:45:10 -0700

    leetcode 1903 - largest odd number in string completed

---
+++ strings/1903-largest-odd-number-in-string.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function (num) {
+  let right = num.length - 1;
+  while (right >= 0) {
+    if (parseInt(num[right]) % 2 !== 0) {
+      return num.slice(0, right + 1);
+    }
+
+    right--;
+  }
+
+  return "";
+};
+
+console.log(largestOddNumber("52"));
+console.log(largestOddNumber("4206"));
+console.log(largestOddNumber("35427"));
+console.log(largestOddNumber("10133890"));
+console.log(largestOddNumber("239537672423884969653287101"));
